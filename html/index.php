4ed18164698438fd0d3e7a063b8fe02f9fc02e52
Alexis ROHEE <email@example.com>
2023-12-07 18:08:34 +0100

    fix : title

--- html/index.php
+++ html/index.php
@@ -2,7 +2,7 @@
 <html lang="en" xmlns="http://www.w3.org/1999/html">
     <head>
         <meta charset="UTF-8">
-        <title>Réseau action Climat</title>
+        <title>Réseau Action Climat</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.4.2/css/all.css" />
         <link rel="stylesheet" href="../css/styles.css"/>
